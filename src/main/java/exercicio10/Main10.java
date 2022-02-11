ee28b13bca1c7a14e8b0acc73f98dabb142d070f
a21nicolasan <a21nicolasan@a26eq04.sanclemente.local>
2022-02-11 11:07:01 +0100

    Ejercicio 10A resuelto

---
+++ src/main/java/exercicio10/Main10.java
@@ -0,0 +1,42 @@
+
+package exercicio10;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.Scanner;
+
+public class Main10 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        TreeSet<String> lista = new TreeSet();
+        
+
+        boolean fin = false;
+        String fin_lista = "_FIN_";
+
+        System.out.println("Introduce texto hasta cuando quieras.");
+        System.out.println("Para parar escribe: _FIN_ ");
+
+        while (fin != true) {
+            String palabra = sc.next();
+            
+            if (palabra.equals(fin_lista)) {
+                fin = true;
+            }
+            else{
+                String palabra_minuscula = palabra.toLowerCase();
+                lista.add(palabra_minuscula);
+            }
+        }
+        
+        Iterator<String> it = lista.iterator();
+        System.out.println("Listado de las palabras:");
+        
+        while (it.hasNext()) {
+            String next = it.next();
+            System.out.println(next);
+        }
+    }
+}
